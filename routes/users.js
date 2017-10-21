aa062e5c10c595df8b776bb8349e3ffe1312d7d4
Teo <teodor08@live.com>
2017-10-21 03:16:06 -0400

    Prevent users registering with the same email address

--- routes/users.js
+++ routes/users.js
@@ -26,21 +26,37 @@ class Users {
     signUp(req, res) {
         const self = this;
 
-        bcrypt.hash(req.body.password, 10, function(err, hash) {
-            if (err) throw err;
+        const queryEmail = {
+            query: 'SELECT * FROM root r WHERE r.email = @email',
+            parameters: [{
+                name: '@email',
+                value: req.body.email
+            }]
+        };
 
-            let user = {
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
-                email: req.body.email,
-                password: hash
-            };
+        self.userDao.find(queryEmail, function (err, items) {
+            if (err) throw (err);
 
-            self.userDao.addItem(user, function (err) {
-                if (err) throw (err);
+            if (items.length > 0) {
+                res.status(406).json({message: "Email already exists"})
+            } else {
+                bcrypt.hash(req.body.password, 10, function (err, hash) {
+                    if (err) throw err;
 
-                res.status(201).json();
-            });
+                    let user = {
+                        firstName: req.body.firstName,
+                        lastName: req.body.lastName,
+                        email: req.body.email,
+                        password: hash
+                    };
+
+                    self.userDao.addItem(user, function (err) {
+                        if (err) throw (err);
+
+                        res.status(201).json();
+                    });
+                });
+            }
         });
     }
 }
